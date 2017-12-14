2bf1e77509d10013583cfb8c72bf3ba6e492b2a0
Paulo Henrique <paulocoliver@gmail.com>
2017-12-14 09:26:44 -0200

    * 'master' of github.com:paulocoliver/gc-laravel-dynamic-attributes:   Correção na criação de atributos com variação Deve-se fornecer o valor has_variations == true | yes para criar um atributo com variação

--- src/Exceptions/AttributeException.php
+++ src/Exceptions/AttributeException.php
@@ -1,15 +1,15 @@
 <?php //004fb
 if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo("Site error: the ".(php_sapi_name()=='cli'?'ionCube':'<a href="http://www.ioncube.com">ionCube</a>')." PHP Loader needs to be installed. This is a widely used PHP extension for running ionCube protected PHP code, website security and malware blocking.\n\nPlease visit ".(php_sapi_name()=='cli'?'get-loader.ioncube.com':'<a href="http://get-loader.ioncube.com">get-loader.ioncube.com</a>')." for install assistance.\n\n");exit(199);
 ?>
-HR+cP+yi3plO2/J/cwMNcXZJsBqMrqk/JKBJbi622vFrO8ZM/I7dYW/YzfbMyOMECOE6v90OCTGW
-AJ9pLmxnVWg6EvDmoVOUB9g3krgX8AhtWhE1f0YfQpvN3yhopuvCCVSLAI52Oze165d40ShmzJ1z
-1yQ0Dgwoc7l50uIbVam4qyUuEMnSTLzD4tplOkO0wpzHniggbseS0B4Fn3gDEBKrUo5LB1BhQUFc
-lzcr2YFO1flAB1vtkn7sQM+Ztvr/wgqoMeHr6v/1AREEJCOs8J9qVHpUfFYCPoZ7svnAot7s28U2
-r9YgJlz+yxK32HZLjuqkA5gElEu1pUBVK2izIZwqnRTZVPUfvHLokD/x+eQ8/lL8/qwvAeKrYIlg
-1uqT5mBy9GyiTzL6OCeOd8fxmAju5KBUiuENz29UH4PjMWUPVJrTq6IzjVAfwGtpzqe/3TbM80WK
-HzUoLhZM9NO52ONB+NO9sTNIFkvZbaEFrsR7K3k29oaGl0qXecoTeF67Xzj8hOnw8YddeSjsl9Vl
-6rqOU5qvAB6/mEU2P70pgA1VsJrQyF1TyJMjAPZyikCzUIdB+fVmOqt36fjFBIcIZ7uwWT1EysFe
-tiMJEvxa4Ahv/22DJR8PbwKgfoG8MK+U1ILaIBIUv2n21icMnfVXLeBkR7ikGTynT5H1SMpvbkLG
-t9Fx/C6P7/b6UAuo+T2zQhPp0m1xG6a0qtXjJoTqXWWJWPUUCBR/QUAkGoWHIeD6MjVeUQWDxF4o
-/MK2g9DapSXZqGBLj9WKLjbh9jfSrspQXgRijTNA4CdHLhEkU9jIcIzOhRSHPKumCUZSlL+biSOV
-Lm==
+HR+cPx3bTu0mC4oepLxYewmhvjUdKpwKi3BCyD54nfBjB2T4f7JKM6GRAH4cdTiJXNZiI4Hxk+pb
+JOod8R/iparJ1fRo3nCBlMs/YGL3ONK8k3P0JiyOc518GHo1hcoH8bp67LFaGtbDzsMmd62bTHAd
+YleLz5Jk4SIUpwMAo5IJ+4b8ROjnLLLLGuK6M9cd0gpmWsqLKslTPmpcA68uZur0hHD6zdtVI7ky
+nAKn3uxgDcN8LfH2wv7ijPov5aWwalPDGTx+6G2aY1slaGkJJoAtHQtCI4BfSkXccMWgBUuq8x7t
+azAz0MvlDwAEN62BmJxQNZr1lt3V8nr4NXSrbf7JZWu06lvJbu0szGsNdRGXuXM8f9iRyxml1e9j
+DeSiQwgFPYW3QifGY1u8BfT9lqMfrVXPmhpfZoe89XZ3vBXX0TUVZ9jX+3D7C5q5R8x/fc+/5byJ
+UHX4zTo8+n2D7EPSb4QNLYDOGmvPbIhY/G0Jr9GjSbgErIxolGmkeHfFFnfidn5H2RP0y3Q4XwEV
+p+LSkJD5XZKDGsixpgt9huEYwWNlAQtv5DA/Bkxx1HDpJAJc3NJSFou7MX386yx5FjXnKyLT/wqP
+5HB5jfLqh/n1dOO/6yqnl8L2KXCBrjQQ4oYZXJYtpSTBZdnlaC4g1aLRRaNygnuxHNijPvC5S+s6
+Nur0h4R89RuNI5W5MGhi0vDmST8qSJB6yHbUir0WAicCKSQbCxeEEk2Oyj94B3cYAQTf0PA9Cdf4
+8HGPzUSA5Xsh6Ml6nw0JlhD0DVvkg/y/aVr5TzUcmQNke12bcGlZc+H2TyNvKp+KGxHPAFVMuu6Q
+03+1u17J2F5wwyY/4xmSX0==
